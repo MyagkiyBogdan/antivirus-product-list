82290343008b230ce9da9f5490404a65b7fbe94e
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-08-17 18:30:29 +0300

    added all files

---
+++ js/fetchProducts.js
@@ -0,0 +1,13 @@
+// export default function fetchProducts(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .catch(error => console.log(error.message));
+// }
+
+// const BASE_URL = 'https://veryfast.io/t/front_test.json';
+// const result = fetchProducts(BASE_URL).then(a => console.log(a));
